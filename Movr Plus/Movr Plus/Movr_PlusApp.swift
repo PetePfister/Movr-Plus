3dee5ae64a34eee2a1a1a5358d97252ff689f6d9
PetePfister <Pfister.Pete@gmail.com>
2025-07-23 14:09:43 -0400

    Add files via upload

---
+++ Movr Plus/Movr Plus/Movr_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movr_PlusApp.swift
+//  Movr Plus
+//
+//  Created by Pete Pfister on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Movr_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
